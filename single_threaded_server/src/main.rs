d0a132c5a4daecab53ebfb31e619416219c55932
Ansh Kumar <ansh0kumar07@gmail.com>
2025-08-22 02:56:30 +0530

    fix

--- single_threaded_server/src/main.rs
+++ single_threaded_server/src/main.rs
@@ -1,6 +1,5 @@
 use std::{
     env,
-    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
@@ -24,16 +23,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "found.html")
+    let (status_line, contents) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", include_str!("found.html"))
     } else {
-        ("HTTP/1.1 400 NOT FOUND", "notfound.html")
+        ("HTTP/1.1 400 NOT FOUND", include_str!("notfound.html"))
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let lenght = contents.len();
+//    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {lenght}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
